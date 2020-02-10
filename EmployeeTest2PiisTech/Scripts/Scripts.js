e3cb17910b1511613f7557ab2b0f22df4769da15
brintasabit <brintasabit@gmail.com>
2020-02-10 20:13:02 +0600

    Backed up partial

--- EmployeeTest2PiisTech/Scripts/Scripts.js
+++ EmployeeTest2PiisTech/Scripts/Scripts.js
@@ -46,35 +46,35 @@ var AddOrEditEmployee = function() {
 		processData: false
 	});
 }
-//function jQueryAjaxPost(form) {
-//	$.validator.unobtrusive.parse(form);
-//	if ($(form).valid()) {
-//		var ajaxConfig = {
-//			type: 'POST',
-//			url: form.action,
-//			data: new FormData(form),
-//			success: function (response) {
-//				if (response.success) {
-//					$("#firstTab").html(response.html);
-//					refreshAddNewTab($(form).attr('data-resetUrl'), true);
-//					$.notify(response.message, "success");
-//					if (typeof activatejQueryTable !== 'undefined' && $.isFunction(activatejQueryTable))
-//						activatejQueryTable();
-//				}
-//				else {
-//					$.notify(response.message, "error");
-//				}
-//			}
-//		}
-//		if ($(form).attr('enctype') == "multipart/form-data") {
-//			ajaxConfig["contentType"] = false;
-//			ajaxConfig["processData"] = false;
-//		}
-//		$.ajax(ajaxConfig);
+function jQueryAjaxPost(form) {
+	$.validator.unobtrusive.parse(form);
+	if ($(form).valid()) {
+		var ajaxConfig = {
+			type: 'POST',
+			url: form.action,
+			data: new FormData(form),
+			success: function (response) {
+				if (response.success) {
+					$("#firstTab").html(response.html);
+					refreshAddNewTab($(form).attr('data-resetUrl'), true);
+					$.notify(response.message, "success");
+					if (typeof activatejQueryTable !== 'undefined' && $.isFunction(activatejQueryTable))
+						activatejQueryTable();
+				}
+				else {
+					$.notify(response.message, "error");
+				}
+			}
+		}
+		if ($(form).attr('enctype') == "multipart/form-data") {
+			ajaxConfig["contentType"] = false;
+			ajaxConfig["processData"] = false;
+		}
+		$.ajax(ajaxConfig);
 
-//	}
-//	return false;
-//}
+	}
+	return false;
+}
 
 function refreshAddNewTab(resetUrl, showViewTab) {
 	$.ajax({
